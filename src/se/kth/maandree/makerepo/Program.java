9ce13ce213718afd61b39f2c0a3c189e2d348270
Mattias Andrée <maandree@operamail.com>
2012-10-25 15:41:41 +0200

    main class base

---
+++ src/se/kth/maandree/makerepo/Program.java
@@ -0,0 +1,54 @@
+/**
+ * makerepo – git repository generator
+ * 
+ * Copyright © 2012  Mattias Andrée (maandree@kth.se)
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package se.kth.maandree.makerepo;
+
+import java.io.*;
+import java.util.*;
+
+
+/**
+ * This is the main class of the program
+ * 
+ * @author  Mattias Andrée, <a href="mailto:maandree@kth.se">maandree@kth.se</a>
+ */
+public class Program
+{
+    /**
+     * Hidden constructor
+     */
+    private Program()
+    {
+	//Nullify default constructor
+    }
+    
+    
+    
+    /**
+     * This is the main entry point of the program
+     *
+     * @param   args       Start up arguments (unused)
+     * @throws  Throwable  On any error
+     */
+    public static void main(final String... args) throws Throwable
+    {
+	System.out.println("...");
+    }
+    
+}
+
